6ba5f4fc91bb9ea7acf919765f8ee26e014d08a2
patrickForster <forster.patrick@my.jcu.edu.au>
2018-08-21 10:27:44 +1000

    refactor password check

---
+++ prac_03/password_entry.py
@@ -0,0 +1,23 @@
+# password checker
+def main():
+    MIN_LENGTH = 4
+    print("Please enter a valid password")
+    print("Your password must be at least", MIN_LENGTH, "characters long")
+    password = get_password()
+    while len(password) < MIN_LENGTH:
+        print("Not enough characters!")
+        password = get_password()
+    print(password_asterisks(password))
+
+
+def get_password():
+    password = input("> ")
+    return password
+
+
+def password_asterisks(password):
+    hidden_password = len(password) * '*'
+    return hidden_password
+
+
+main()
