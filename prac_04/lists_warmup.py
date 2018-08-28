9f76ff17ab3c694c3632d3cb6fce8e20aa22403c
patrickForster <forster.patrick@my.jcu.edu.au>
2018-08-28 10:11:24 +1000

    complete list comprehensions

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -7,7 +7,7 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # 5 in numbers = True
 # 7 in numbers = False
 # "3" in numbers = False
-# numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2]
+# numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 
 # Question 1
 numbers[0] = 10
